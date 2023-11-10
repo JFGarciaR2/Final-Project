cc9ae708c2929e8b23c1ce1b75086a069af0b07c
Juan Fernando Garcia Restrepo <juan.garcia36856@ucaldas.edu.co>
2023-11-10 09:00:34 -0500

    Create a Firsts Class

---
+++ Main.java
@@ -0,0 +1,15 @@
+import java.sql.SQLOutput;
+
+public class Main {
+    public static void main(String[] args) {
+        Patients patient1 = new Patients();
+        patient1.setPatientId(1);
+        patient1.setPatientName("John");
+        patient1.setPatientAddress("123 Main St");
+        patient1.setPatientPhone("123-456-7890");
+        patient1.setPatientEmail("");
+
+        System.out.println("Patient ID: " + patient1.getPatientId());
+        System.out.println("Patient Name: " + patient1.getPatientName());
+    }
+}
