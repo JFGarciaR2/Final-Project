f8512501ac3865e4e80961ff9f2276d445f6434f
Juan Fernando Garcia Restrepo <juan.garcia36856@ucaldas.edu.co>
2023-12-07 23:55:37 -0500

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -1,89 +1,96 @@
-//Author: Juan Fernando Garcia Restrepo
-
-import java.io.IOException;
 import java.util.Scanner;
 
 public class Main {
+
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
+
+        // Create instances of CSVCoder
         CSVCoder<Person> personCSVCoder = new PersonCSVCoder(';');
         CSVCoder<Dietitian> dietitianCSVCoder = new DietitianCSVCoder(';');
         CSVCoder<Patients> patientsCSVCoder = new PatientsCSVCoder(';');
         CSVCoder<Meal> mealCSVCoder = new MealCSVCoder(';');
         CSVCoder<DietPlan> dietPlanCSVCoder = new DietPlanCSVCoder(';');
 
-        while (true) {
-            System.out.println("Menu:");
-            System.out.println("1. Agregar nuevo Nutricionista");
-            System.out.println("2. Agregar nuevo Paciente");
-            System.out.println("3. Agregar nueva Comida");
-            System.out.println("4. Agregar nuevo Plan de Dieta");
-            System.out.println("5. Mostrar información");
-            System.out.println("6. Salir");
+        // Read data from CSV file
+        try {
+            personCSVCoder.readFromFile("DatesForProject.csv", Person.getPersonList());
+            dietitianCSVCoder.readFromFile("DatesForProject.csv", Dietitian.getDietitianList());
+            patientsCSVCoder.readFromFile("DatesForProject.csv", Patients.getPatientsList());
+            mealCSVCoder.readFromFile("DatesForProject.csv", Meal.getMealList());
+            dietPlanCSVCoder.readFromFile("DatesForProject.csv", DietPlan.getDietPlanList());
+        } catch (Exception e) {
+            System.out.println("Error al leer desde el archivo CSV: " + e.getMessage());
+        }
 
-            int choice = scanner.nextInt();
-            scanner.nextLine(); // Consume the \n character
+        //Menu
+        int option;
+        do {
+            System.out.println("\n*** Menú Principal ***");
+            System.out.println("1. Gestionar Personas");
+            System.out.println("2. Gestionar Dietitian");
+            System.out.println("3. Gestionar Patients");
+            System.out.println("4. Gestionar Meals");
+            System.out.println("5. Gestionar Diet Plans");
+            System.out.println("0. Salir");
+            System.out.print("Ingrese la opción: ");
+            option = scanner.nextInt();
+            scanner.nextLine(); 
 
-            switch (choice) {
+            switch (option) {
                 case 1:
-                    addDietitian(scanner, dietitianCSVCoder);
+                    managePersons(scanner, personCSVCoder);
                     break;
                 case 2:
-                    addPatient(scanner, patientsCSVCoder);
+                    manageDietitians(scanner, dietitianCSVCoder);
                     break;
                 case 3:
-                    addMeal(scanner, mealCSVCoder);
+                    managePatients(scanner, patientsCSVCoder);
                     break;
                 case 4:
-                    addDietPlan(scanner, dietPlanCSVCoder);
+                    manageMeals(scanner, mealCSVCoder);
                     break;
                 case 5:
-                    displayInformation();
+                    manageDietPlans(scanner, dietPlanCSVCoder);
                     break;
-                case 6:
-                    saveDataToCSV(personCSVCoder, dietitianCSVCoder, patientsCSVCoder, mealCSVCoder, dietPlanCSVCoder);
-                    System.out.println("Saliendo del programa.");
-                    System.exit(0);
+                case 0:
+                    System.out.println("Saliendo del programa...");
+                    break;
                 default:
-                    System.out.println("Opción no válida. Inténtalo de nuevo.");
+                    System.out.println("Opción no válida. Inténtelo de nuevo.");
             }
-        }
-    }
 
-    private static void addDietitian(Scanner scanner, CSVCoder<Dietitian> dietitianCSVCoder) {
-        // Aquí puedes solicitar información al usuario para crear un nuevo Dietitian
-        // Luego, crea una instancia de Dietitian y agrégala a la lista correspondiente
-    }
+        } while (option != 0);
 
-    private static void addPatient(Scanner scanner, CSVCoder<Patients> patientsCSVCoder) {
-        // Similar a addDietitian, pero para Patients
-    }
-
-    private static void addMeal(Scanner scanner, CSVCoder<Meal> mealCSVCoder) {
-        // Similar a addDietitian, pero para Meal
-    }
-
-    private static void addDietPlan(Scanner scanner, CSVCoder<DietPlan> dietPlanCSVCoder) {
-        // Similar a addDietitian, pero para DietPlan
-    }
-
-    private static void displayInformation() {
-        // Aquí puedes mostrar información según tus necesidades
-    }
-
-    private static void saveDataToCSV(CSVCoder<Person> personCSVCoder,
-                                      CSVCoder<Dietitian> dietitianCSVCoder,
-                                      CSVCoder<Patients> patientsCSVCoder,
-                                      CSVCoder<Meal> mealCSVCoder,
-                                      CSVCoder<DietPlan> dietPlanCSVCoder) {
+        // Write data to CSV file
         try {
-            personCSVCoder.writeToFile("persons.csv", Person.getPersonList());
-            dietitianCSVCoder.writeToFile("dietitians.csv", Dietitian.getDietitianList());
-            patientsCSVCoder.writeToFile("patients.csv", Patients.getPatientsList());
-            mealCSVCoder.writeToFile("meals.csv", Meal.getMealList());
-            dietPlanCSVCoder.writeToFile("dietplans.csv", DietPlan.getDietPlanList());
-        } catch (IOException e) {
-            e.printStackTrace();
+            personCSVCoder.writeToFile("DatesForProject.csv", Person.getPersonList());
+            dietitianCSVCoder.writeToFile("DatesForProject.csv", Dietitian.getDietitianList());
+            patientsCSVCoder.writeToFile("DatesForProject.csv", Patients.getPatientsList());
+            mealCSVCoder.writeToFile("DatesForProject.csv", Meal.getMealList());
+            dietPlanCSVCoder.writeToFile("DatesForProject.csv", DietPlan.getDietPlanList());
+        } catch (Exception e) {
+            System.out.println("Error al escribir en el archivo CSV: " + e.getMessage());
         }
+    }
+
+    private static void managePersons(Scanner scanner, CSVCoder<Person> personCSVCoder) {
+        // Implementa lógica para gestionar Personas
+    }
+
+    private static void manageDietitians(Scanner scanner, CSVCoder<Dietitian> dietitianCSVCoder) {
+        // Implementa lógica para gestionar Dietitians
+    }
+
+    private static void managePatients(Scanner scanner, CSVCoder<Patients> patientsCSVCoder) {
+        // Implementa lógica para gestionar Patients
+    }
+
+    private static void manageMeals(Scanner scanner, CSVCoder<Meal> mealCSVCoder) {
+        // Implementa lógica para gestionar Meals
+    }
+
+    private static void manageDietPlans(Scanner scanner, CSVCoder<DietPlan> dietPlanCSVCoder) {
+        // Implementa lógica para gestionar Diet Plans
     }
 }
